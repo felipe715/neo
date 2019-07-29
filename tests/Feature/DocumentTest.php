0d26691b56068b0bd55b78a884ebd9bb4ee2f001
Felipe Aguiar <felipe.aguiar@inv000706.involves.corp>
2019-07-28 23:33:05 -0300

    ajustes testes

--- tests/Feature/DocumentTest.php
+++ tests/Feature/DocumentTest.php
@@ -6,6 +6,7 @@ use App\Document;
 use App\Repositories\DocumentRepository;
 use App\Repositories\DocumentRepositoryInterface;
 use App\Services\DocumentService;
+use PhpParser\Node\Expr\Cast\Object_;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -24,4 +25,41 @@ class DocumentTest extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+     * @expectedException Exception
+     */
+    public function testInsertDocumentWithInvalidCpf()
+    {
+        $d = new DocumentService(new DocumentRepository());
+        $d->save(array('type' => 'cpf', 'number' => '300121212', 'blacklist' => true));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testInsertDocumentWithInvalidCnpj()
+    {
+        $d = new DocumentService(new DocumentRepository());
+        $d->save(array('type' => 'cnpj', 'number' => '300121212', 'blacklist' => true));
+    }
+
+    public function testInsertValidCpf()
+    {
+        $repository = $this->createMock('App\Repositories\DocumentRepository');
+        $repository->method('findBy')->willReturn(array());
+
+        $d = new DocumentService($repository);
+        $this->assertIsObject($d->save(array('type' => 'cpf', 'number' => '30007224915', 'blacklist' => true)));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testInsertDuplicateCpf()
+    {
+        $repository = $this->createMock('App\Repositories\DocumentRepository');
+        $repository->method('findBy')->willReturn(array('type' => 'cpf', 'number' => '30007224915', 'blacklist'));
+        $d = new DocumentService($repository);
+        $d->save(array('type' => 'cpf', 'number' => '30007224915', 'blacklist' => true));
+    }
 }
